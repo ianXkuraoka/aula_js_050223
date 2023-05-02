bfd0fd8bba65dd366a61d8986e7a6910e787fc4e
Ian <ian.xk9@gmail.com>
2023-05-02 11:50:41 -0300

    exercicio_7

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 //declarando data
-
 let data0 =new Date()
 document.write(data0)
 document.write("<br></br>")
 
 //pega somente o dia
-
 let data1 =new Date().getDate()
 document.write(data1)
 document.write("<br></br>")
 
 //pega o dia com o 0 na frente
-
 let data2 =new Date().getDate()
 document.write(('0'+data2).slice(0-2));
 document.write("<br></br>")
@@ -30,3 +27,11 @@ document.write("<br></br>")
 let data6 =new Date()
 let diasSemana=['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
 document.write(diasSemana[data6.getDay()])
+
+//formatação completa de data
+let data7 =new Date()
+let dia =(' 0'+data7.getDate()).slice(0-3)
+let mes =data7.getMonth()
+let meses =['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+let ano =data7.getFullYear()
+document.write(dia+ ' de '+meses[mes]+' de '+ano)
